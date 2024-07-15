8908e3f01af7e0272ff389b44a0088480aa122e2
Ekaterina-spec-6553 <ekaterinarunova6553@gmail.com>
2024-07-16 00:33:35 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 number_of_completed_homeworks = '12'
 number_of_hours_spent = '1,5'
 course_name = 'Python'
-time_for_one_lesson = '0,125'
+time_for_one_lesson = 1.5 / 12
 print('Курс:', course_name, ", всего задач:", number_of_completed_homeworks, ", затрачено часов:", number_of_hours_spent,", среднее время выполнения", time_for_one_lesson, "часа." )
